bd5a50d5e32201dbcf13a1cf46d66bf4bcce31e2
minseok <imuchgabis@gmail.com>
2024-03-05 19:23:00 +0900

    별점체크

--- ReactUi/src/App.jsx
+++ ReactUi/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import StarRating from "./components/starRating";
 
 function App() {
   return (
     <>
       <Accordian />
+      <StarRating />
     </>
   );
 }
