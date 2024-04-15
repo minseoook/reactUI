547c48aca9b1ea9ded773d13dadd94c8aa4c8b6d
minseok <imuchgabis@gmail.com>
2024-04-15 15:53:32 +0900

    scroll값 사용

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Accordian from "./components/accordian";
 import ImageSlider from "./components/imageSlider";
 import LightDarkMode from "./components/light-dark-mode";
 import LoadMoreButton from "./components/loadMoreButton";
+import ScrollIndicator from "./components/scroll-indicator";
 
 function App() {
   return (
     <div>
-      <Accordian />
+      {/* <Accordian />
       <ImageSlider url={"https://picsum.photos/v2/list"} />
       <LoadMoreButton />
-      <LightDarkMode />
+      <LightDarkMode /> */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
   );
 }
