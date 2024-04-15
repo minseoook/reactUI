db24d14b57399adb933650132b64acbc9d99f288
minseok <imuchgabis@gmail.com>
2024-04-15 18:01:13 +0900

    검색어 자동완성 체인지이벤트안에서 전체데이터 조회

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LightDarkMode from "./components/light-dark-mode";
 import LoadMoreButton from "./components/loadMoreButton";
 import Modaltest from "./components/modal/modaltest";
 import ScrollIndicator from "./components/scroll-indicator";
+import SearchAutoComplete from "./components/search-autocomplete";
 import Tab from "./components/tab";
 import TabTest from "./components/tab/tab-test";
 
@@ -17,7 +18,8 @@ function App() {
       <LightDarkMode /> */}
       {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> */}
       {/* <TabTest /> */}
-      <Modaltest />
+      {/* <Modaltest /> */}
+      <SearchAutoComplete />
     </div>
   );
 }
