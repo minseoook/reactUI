be02d4540b03f53de6770eeca7054f31d4dc5bd9
minseok <imuchgabis@gmail.com>
2024-04-22 19:29:43 +0900

    페이지네이션

---
+++ reactUi2/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PaginationTest from "./components/pagination/test";
+
+function App() {
+  return (
+    <>
+      <PaginationTest />
+    </>
+  );
+}
+
+export default App;
