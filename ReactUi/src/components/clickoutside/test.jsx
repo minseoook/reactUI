0e9647ade243c32f4c98d2435ca7cdb089b859c7
minseok <imuchgabis@gmail.com>
2024-04-24 18:52:14 +0900

    바깥 클릭 화면 닫힘 훅 설명

--- ReactUi/src/components/clickoutside/test.jsx
+++ ReactUi/src/components/clickoutside/test.jsx
@@ -4,18 +4,21 @@ import useOutsideClick from ".";
 export default function UseOnclickOutsideTest() {
   const [showContent, setShowContent] = useState(false);
   const ref = useRef();
+
   useOutsideClick(ref, () => setShowContent(false));
 
   return (
     <div>
-      {showContent ? (
+      <button onClick={() => setShowContent(!showContent)}>
+        {showContent ? "Hide Content" : "Show Content"}
+      </button>
+      {showContent && (
         <div ref={ref}>
           <h1>아무거나 아무거나</h1>
           <p>안녕안녕안뇨ㅕㅇㅇㄴ훈림ㄴ라ㅣㅠㄴ아.ㅓ흄라</p>
         </div>
-      ) : (
-        <button onClick={() => setShowContent(true)}>Show Content</button>
       )}
     </div>
   );
 }
+//토글 버튼으로 클릭마다 컨텐츠 보이게
