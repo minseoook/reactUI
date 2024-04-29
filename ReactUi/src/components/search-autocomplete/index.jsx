640362dff4b6cbef11f988987f9282a4f76259df
minseok <imuchgabis@gmail.com>
2024-04-29 12:54:18 +0900

    검색어 자동완성 정리

--- ReactUi/src/components/search-autocomplete/index.jsx
+++ ReactUi/src/components/search-autocomplete/index.jsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import DropDown from "./dropDown";
 
+// 일단 먼저 상태값 생각하자
+// 상태는 검색을 위한 상태, 그리고 검색어로 filter가 있으면 아래에 리스트를 보여주기 위한 불리언 상태
+// 그리고 필터된 유저를 위한 상태
+// 온체인지에서 검색어 변할때마다 필터하고 필터된 배열을 저장하고 그걸 리스트로 뿌려준다
+// 만약 리스트가 클릭이되면 검색어 변경하고 필터배열 없애고, 불리언도 false로 한다
+// 프랍스로 내려주긴 많아서 부모에서 클릭메서드 처리하고 자식에서 클릭된걸 알려면
+// e.target.innerText를 사용한다
 const SearchAutoComplete = () => {
   const [users, setusers] = useState([]);
   const [search, setsearch] = useState("");
