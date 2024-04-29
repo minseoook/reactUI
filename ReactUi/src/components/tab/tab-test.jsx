7f137e4e203c57c49261c2a916e21c457ce9c8ca
minseok <imuchgabis@gmail.com>
2024-04-29 13:01:51 +0900

    탭 컴포넌트 정리

--- ReactUi/src/components/tab/tab-test.jsx
+++ ReactUi/src/components/tab/tab-test.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import "./index.css";
 import Tab from "./index";
 
+// 상태를 인덱스로 받아서 탭이 눌리면 부모의 상태가 변하고
+// 그로인해 각 상태마다 적합한 데이터가 보여집니다
+// 그리고 인덱스를 자식 탭으로 내려줘서 내려준 인덱스와 맞는 탭을 액티브 상태로 합니다
 export default function TabTest() {
   const [index, setIndex] = useState(null);
   const tabs = [
